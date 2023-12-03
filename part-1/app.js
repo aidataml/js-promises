1082a98676b60999b0fa9c3651a72b2615455f80
aidataml <52681210+aidataml@users.noreply.github.com>
2023-12-03 13:53:35 -0500

    Initial upload of JavaScript Promises (part 1)

--- part-1/app.js
+++ part-1/app.js
@@ -1,1 +1,28 @@
+let favoriteNumber = 10;
+let baseURL = "http://numbersapi.com";
 
+/* Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. 
+(Make sure you get back JSON by including the json query key, specific to this API.*/
+
+$.getJSON(`${baseURL}/${favoriteNumber}?json`).then(data => {
+  console.log(data);
+});
+
+/* Figure out how to get data on multiple numbers in a single request. Make that request and when you 
+get the data back, put all of the number facts on the page.*/
+
+let favoriteNumbers = [7, 11, 22];
+$.getJSON(`${baseURL}/${favoriteNumbers}?json`).then(data => {
+  console.log(data);
+});
+
+/* Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. 
+It’s okay if some of the facts are repeats.*/
+
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favoriteNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
